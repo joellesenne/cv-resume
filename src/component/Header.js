8f60c3b55ea7f98192e55a6082cba64af535ad67
Joël Lesenne <jfkles@me.com>
2020-01-10 05:19:21 +0100

    ✏️ Fixing typos.

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
 			<img src={Avatar} alt="Avatar" />
 		</picture>
 		<h1>Joël <br /> Lesenne</h1>
-		<p>Devenir développeur Web</p>
+		<p>Développeur Web</p>
 		<a className="email" href="mailto:contact@joellesenne.dev">contact@joellesenne.dev</a>
 		<a className="phone" href="tel:+33611578371">+33 611 578 371</a>
 		<a className="site" href="https://joellesenne.dev" target="_blank" rel="noopener noreferrer">joellesenne.dev</a>
