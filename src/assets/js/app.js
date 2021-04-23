06c7e99e64f3a947a839a1b5434a611647425914
joellesenne <jfkles@me.com>
2021-04-23 08:10:03 +0200

    cookie: add react-cookie-content and google-analytic for RGPD made in french

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CookieConsent from "react-cookie-consent";
+import ReactGA from 'react-ga';
+
 
 import '../css/app.css'
 
@@ -9,11 +12,25 @@ import Footer from '../../component/Footer'
 
 const App = () => (
   <>
-    <Header />
-    <Article />
-    <Footer />
+	  <Header />
+	  <Article />
+	  <Footer />
+	  <CookieConsent
+		  enableDeclineButton
+		  location="bottom"
+		  buttonText="Accepter"
+		  declineButtonText="Refuser"
+		  cookieName="myAwesomeCookieName2"
+		  style={{ backgroundColor: "var(--colorPrimary)" }}
+		  declineButtonStyle={{backgroundColor: "#eb5757", color: "var(--colorWhite)"}}
+		  buttonStyle={{ color: "var(--colorWhite)", fontSize: "13px", backgroundColor: "#27ae60" }}
+		  expires={150}
+	  >Ce site utilise des cookies pour assurer le bon fonctionnement des procédures et améliorer l'expérience de l'utilisation d'applications en ligne.</CookieConsent>
   </>
 )
+
+ReactGA.initialize('G-F50P1V1387');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 let app = document.getElementById('app')
 
