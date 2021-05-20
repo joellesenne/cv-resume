599cebf3488096fae1d9f8cca5cc2c1ae9031391
joellesenne <jfkles@me.com>
2021-05-20 19:22:23 +0200

    :seedling: update seed files

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import CookieConsent from "react-cookie-consent";
 import ReactGA from 'react-ga';
 
-import '../css/app.css'
+import './assets/css/app.css'
 
-import Header from '../../component/Header'
-import Article from '../../component/Article'
-import Footer from '../../component/Footer'
+import Header from './components/Header'
+import Article from './components/Article'
+import Footer from './components/Footer'
 import CarbonBadge from "react-carbonbadge";
 
 const App = () => (
@@ -26,7 +26,7 @@ const App = () => (
 		  declineButtonStyle={{backgroundColor: "#eb5757", color: "var(--colorWhite)"}}
 		  buttonStyle={{ color: "var(--colorWhite)", fontSize: "13px", backgroundColor: "#27ae60" }}
 		  expires={150}
-	  >Ce site utilise des cookies pour assurer le bon fonctionnement des procédures et améliorer l'expérience de l'utilisation d'applications en ligne.</CookieConsent>
+	  >Ce site utilise des cookies pour assurer le bon fonctionnement des procédures et améliore l'expérience de l'utilisation d'applications en ligne.</CookieConsent>
   </>
 )
 
